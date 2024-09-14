35c2a41a0b0b5097877edffd3788ca25d953aa6b
Bilal Patel <bilalpatel@outlook.com>
2024-09-14 16:47:02 -0400

    Initialize inventory with product objects

--- inventory_management.js
+++ inventory_management.js
@@ -1,8 +1,14 @@
+// Task 1 - Create an Inventory Array of Product Objects
+
+
 let inventory = [
     { name: 'apple', price: 5, quantity: 100, lowStockLevel: 20 },
-    { name: 'orange', price: 4, quantity: 200, lowStockLevel: 10 },
-    { name: 'banana', price: 3, quantity: 300, lowStockLevel: 30 },
+    { name: 'orange', price: 4, quantity: 200, lowStockLevel: 20 },
+    { name: 'banana', price: 3, quantity: 300, lowStockLevel: 20 },
     { name: 'pear', price: 2, quantity: 500, lowStockLevel: 20 },
-    { name: 'grape', price: 1, quantity: 425, lowStockLevel: 15 }
+    { name: 'grape', price: 1, quantity: 15, lowStockLevel: 20 }
 ];
-// console.log (inventory)
+//console.log (inventory)
+
+
+
